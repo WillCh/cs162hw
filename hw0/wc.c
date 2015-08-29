534441303101b0d4f436b6726c02e3d2fd742992
willch <williamchenthu@gmail.com>
2015-08-29 20:30:56 +0000

    revised the stdio

--- hw0/wc.c
+++ hw0/wc.c
@@ -6,15 +6,37 @@ int main (int argc, char* argv[]) {
     int charCount = 0;
     int wordCount = 0;
     if (argc <= 1) {
+		char ch;
+		char prevCh = ' ';
+		while ((ch = getchar()) != EOF) {
+			charCount++;
+            if (prevCh == ' ' && ch != ' ') {
+				wordCount++;
+             }
+		// handle the line change
+		if (ch == '\n' || ch == '\r') {
+			lineCount++;
+        }
+			prevCh = ch;
+        }
+        // output the results
         printf("%d %d %d\n", lineCount, wordCount, charCount);
         return 0;
-    }    
+    }
+	// for debug
+	printf("there are %d params \n", argc);
+	// these params are：
+	for (int i = 0; i < argc; i++) {
+		printf("%s\n", argv[i]);
+	}    
 	char *fileName;
 	if (argc == 2) {    
 		fileName = argv[1];
     } else if (argc > 2) {
 		fileName = argv[2];
 	}
+	// for debug
+	printf("the file name is %s\n", fileName);
 	FILE *inputFilePointer = fopen(fileName, "r");
     if (inputFilePointer == NULL) {
         fprintf(stderr, "Can't open input file!\n");
